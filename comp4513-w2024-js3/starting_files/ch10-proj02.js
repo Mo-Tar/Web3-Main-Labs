18029fb69c2aa57a53b668036bb1d2dc13c0c3bb
Mo-Tar <muhammadtarar194@gmail.com>
2024-01-16 18:40:59 -0700

    step 4

--- comp4513-w2024-js3/starting_files/ch10-proj02.js
+++ comp4513-w2024-js3/starting_files/ch10-proj02.js
@@ -1,11 +1,14 @@
+import { Play, Act, Scene } from ".play-module.js";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const url =
+    "https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php";
 
-document.addEventListener("DOMContentLoaded", function() {
-
-	
-	const url = 'https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php';
-
-   /*
+   let data;
+   let play;
+   let acts;
+   let scenes;
+  /*
      To get a specific play, add play name via query string, 
 	   e.g., url = url + '?name=hamlet';
 	 
@@ -13,11 +16,23 @@ document.addEventListener("DOMContentLoaded", function() {
 	 https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=jcaesar
      
    */
-	 
-   
-    /* note: you may get a CORS error if you test this locally (i.e., directly from a
+  async function myfetcher(playname) {
+    const response = await fetch(url + `?name=${playname}`);
+    data = await response.json();
+    populatePage();
+  }
+
+  /* note: you may get a CORS error if you test this locally (i.e., directly from a
        local file). To work correctly, this needs to be tested on a local web server.  
        Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
        use built-in Live Preview.
     */
+
+  let firstSelect = document.querySelector("#playList");
+
+  firstSelect.addEventListener("change", (e) => {
+    myfetcher(e.target.value);
+  });
+
+  function populatePage() {}
 });
--- comp4513-w2024-js3/starting_files/play-module.js
+++ comp4513-w2024-js3/starting_files/play-module.js
@@ -1,2 +1,26 @@
 /* In this module, create three classes: Play, Act, and Scene. */
 
+class Play {
+  constructor(title, short, acts, personas) {
+    this.title = title;
+    this.short = short;
+    this.acts = acts;
+    this.personas = personas;
+  }
+}
+
+class Act {
+  constructor(name, scenes) {
+    this.name = name;
+    this.scenes = scenes;
+  }
+}
+
+class Scene {
+  constructor(name, title, stageDirection, speeches) {
+    this.name = name;
+    this.title = title;
+    this.stageDirection = stageDirection;
+    this.speeches = speeches;
+  }
+}
